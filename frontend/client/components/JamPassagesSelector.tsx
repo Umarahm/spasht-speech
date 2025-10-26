763384e13ccd3d7829e5c8d7e3d89f131bb1ca9f
Umarahm <umarahmed730@gmail.com>
2025-10-26 11:57:15 +0530

    MobileFixes

--- frontend/client/components/JamPassagesSelector.tsx
+++ frontend/client/components/JamPassagesSelector.tsx
@@ -123,3 +123,4 @@ export default function JamPassagesSelector({ open, onOpenChange }: JamPassagesS
 
 
 
+
--- frontend/client/components/ProgressSelector.tsx
+++ frontend/client/components/ProgressSelector.tsx
@@ -123,3 +123,4 @@ export default function ProgressSelector({ open, onOpenChange }: ProgressSelecto
 
 
 
+
